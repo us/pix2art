c5cba887540f8991cb90428df03ee3cc343de239
Mustafa Çalap <mustafa@calap.co>
2020-06-01 00:41:30 +0300

    Add arguments parser

--- detect_edges_image.py
+++ detect_edges_image.py
@@ -4,7 +4,14 @@ import numpy as np
 from os import listdir
 import random
 import tensorflow as tf
+import argparse
 
+parser = argparse.ArgumentParser(description='Pix2Art AML Final Project')
+parser.add_argument('-p','--dataset-path',dest="dataset_path", type=str, help='Dataset path for cleaning')
+parser.add_argument('-o','--output-path',dest="output_path", type=str, help='Dataset output path')
+parser.add_argument('-r', '--run',dest="run", help='Run directly from webcam stream', action='store_true')
+
+args = parser.parse_args()
 
 class CropLayer(object):
     def __init__(self, params, blobs):
@@ -88,7 +95,7 @@ def image_resize(image, width=256, height=256, inter=cv2.INTER_AREA):
 
 # resize, crop and apply hed method in this order
 # then concatenate hed image and cropped size side by side
-def convertImages(path):
+def convertImages(path, outputPath):
     for index, imageFile in enumerate(folderList(path + "/*.jpg")):
         img = cv2.imread(imageFile)
         resizedImage = image_resize(img)
@@ -96,7 +103,7 @@ def convertImages(path):
         hedImage = hedConvert(croppedImage)
         hedImage = np.stack((hedImage, ) * 3, axis=-1)
         concatedImage = np.concatenate((hedImage, croppedImage), axis=1)
-        cv2.imwrite("dataset/" + str(index + 1) + ".jpg", concatedImage)
+        cv2.imwrite(outputPath + "/" + str(index + 1) + ".jpg", concatedImage)
         print(imageFile.split("/")[-1])
 
 
@@ -125,12 +132,10 @@ def load_graph(frozen_graph_filename):
             tf.import_graph_def(od_graph_def, name='')
     return graph
 
-
-# use webcam capture device
-cap = cv2.VideoCapture(0)
-
 # apply data cleaning and preprocessing methods to webcam feed
-if __name__ == "__main__":
+def runWebcamStream():
+    # use webcam capture device
+    cap = cv2.VideoCapture(0)
     graph = load_graph("frozen_model.pb")
     image_tensor = graph.get_tensor_by_name('image_tensor:0')
     output_tensor = graph.get_tensor_by_name('generate_output/output:0')
@@ -154,3 +159,13 @@ if __name__ == "__main__":
             break
     cap.release()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+
+    if args.run:
+        print("Running from webcam stream")
+        runWebcamStream()
+    elif args.dataset_path!=None or args.output_path!=None:
+    	print(f"Cleaning dataset from {args.dataset_path} to {args.output_path}")
+    	convertImages(args.dataset_path, args.output_path)
+
